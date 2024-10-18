0eb6d3b3d6f51f40e6e852d50fe2334195e90e35
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-10-18 12:24:37 +0800

    fix error export member

--- src/health/controllers/health.controller.ts
+++ src/health/controllers/health.controller.ts
@@ -5,7 +5,7 @@ import { RedisHealthIndicator } from '@liaoliaots/nestjs-redis-health';
 import Redis from 'ioredis';
 
 @Controller()
-export class AppController {
+export class HealthController {
   private readonly redis: Redis;
 
   constructor(private readonly health: HealthCheckService, private readonly redisIndicator: RedisHealthIndicator) {

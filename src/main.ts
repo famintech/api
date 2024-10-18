0da50d3bb2784c7a3e17038b2237c64ac1f24bd1
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-10-18 16:24:05 +0800

    saje ubah

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  
-  const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new IoAdapter(app));
 
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.useWebSocketAdapter(new IoAdapter(app));
+  const port = configService.get<number>('port');
+  await app.listen(port);
 
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }

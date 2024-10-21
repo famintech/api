86d6674334b378206094837dc9877fb31affc560
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-10-21 11:29:21 +0800

    fix dependenices

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -3,10 +3,13 @@ import { PermissionsService } from './services/permissions.service';
 import { PermissionsController } from './controllers/permissions.controller';
 import { PrismaService } from '../../prisma/services/prisma.service';
 import { PermissionCacheService } from './services/permissions-cache.service';
+import { RedisModule } from '../redis/redis.module';
+import { RedisService } from '../redis/services/redis.service';
 
 @Module({
+  imports: [RedisModule],
   controllers: [PermissionsController],
-  providers: [PermissionsService, PrismaService, PermissionCacheService],
+  providers: [PermissionsService, RedisService, PrismaService, PermissionCacheService],
   exports: [PermissionsService, PermissionCacheService],
 })
 export class PermissionsModule {}

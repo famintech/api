92ee0ad3652197bf6dbae6c580795ae4edd1a3b7
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-10-21 11:52:07 +0800

    test

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,12 @@ import { RolesModule } from '../roles/roles.module';
 import { RolesService } from '../roles/services/roles.service';
 import { PermissionCacheService } from '../permissions/services/permissions-cache.service';
 import { PermissionsModule } from '../permissions/permissions.module';
+import { RedisModule } from '../redis/redis.module';
+import { RedisService } from '../redis/services/redis.service';
 
 @Module({
-  imports: [RolesModule, PermissionsModule],
-  providers: [UsersService, RolesService, PermissionCacheService],
+  imports: [RolesModule, PermissionsModule, RedisModule],
+  providers: [UsersService, RolesService, PermissionCacheService, RedisService],
   controllers: [UsersController],
   exports: [UsersService],
 })
